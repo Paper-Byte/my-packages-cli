3110c80c1f47f965b5505473e014e6fd1cb76535
Noah <noahhughesph@gmail.com>
2023-10-05 13:10:17 -0400

    Fix Language model name setter and add repr method

--- lib/models/language.py
+++ lib/models/language.py
@@ -8,3 +8,19 @@ class Language:
     def __init__(self, name, id=None):
         self.id = id
         self.name = name
+
+    def __repr__(self):
+        return f"<Language {self.id}: {self.name}>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
