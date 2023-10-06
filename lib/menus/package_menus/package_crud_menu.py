cf262f0c1d726bc798988fa0c9f25902454074e0
Noah <noahhughesph@gmail.com>
2023-10-06 15:42:09 -0400

    update options of packages main menu

--- lib/menus/package_menus/package_crud_menu.py
+++ lib/menus/package_menus/package_crud_menu.py
@@ -1,43 +1,39 @@
 from models.package import Package
 from rich import print
 from menu_helpers.menu_utils import clear_screen
-from ascii_titles import PACKAGE
 import menus.package_menus.packages_sub_menu  # packages_sub_menu()
-import menus.main_menu  # main_menu()
-
-ALL_PACKAGES = Package.get_all()
+import pyfiglet
 
 
-def packages_main_menu():
+def package_crud_menu(package):
+
+    PACKAGE_TITLE = pyfiglet.figlet_format((f'{package.name}'), font='mini')
+
     clear_screen()
-    print(f"[bold dark_turquoise]{PACKAGE}")
+    print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
     while True:
-        packages_main_menu_options()
+        packages_main_menu_options(package)
         choice = input("--> ")
         if ((choice.lower() == "b") or (choice.lower() == "back")):
-            menus.main_menu.main_menu()
-        elif (Package.find_by_id(int(choice))):
-            menus.package_menus.packages_sub_menu.packages_sub_menu(
-                Package.find_by_id(int(choice)))
+            menus.package_menus.packages_sub_menu.packages_sub_menu(package)
         else:
-            print(f"[bold dark_turquoise]{PACKAGE}")
+            print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
             print(":white_exclamation_mark:",
                   "[red blink][bold]Error:[/bold] Invalid option, try again.")
 
 
-def packages_main_menu_options():
+def packages_main_menu_options(package):
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
-    for package in ALL_PACKAGES:
-        print(f"[magenta]*[/magenta]  [dark_turquoise]{package.name}")
+    print(f"[magenta]*[/magenta]  [dark_turquoise]{package.name}")
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
     print("[grey53]*[/grey53]")
     print('[dark_turquoise]*[/dark_turquoise]  [bold][dark_turquoise]"back" [magenta][italic]OR[/italic][/magenta] "b"[/bold][/dark_turquoise][grey53] to return to the previous menu',
           ':right_arrow_curving_left:')
     print(
-        '[grey53]*[/grey53]  [bold][dark_turquoise]"create" [magenta][italic]OR[/italic][/magenta] "c"[/bold][/dark_turquoise][grey53] to create a new package', ':robot:')
+        '[grey53]*[/grey53]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
     print(
-        '[dark_turquoise]*[/dark_turquoise]  [bold][dark_turquoise]"delete" [magenta][italic]OR[/italic][/magenta] "d"[/bold][/dark_turquoise][grey53] to delete a package', ':robot:')
+        '[dark_turquoise]*[/dark_turquoise]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
     print(
-        '[grey53]*[/grey53]  [bold][dark_turquoise]NUM[/bold][/dark_turquoise][grey53] to modify a package', ':robot:')
+        '[grey53]*[/grey53]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
     print("[dark_turquoise]*[/dark_turquoise]")
     print("[grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise]")
--- lib/menus/package_menus/packages_sub_menu.py
+++ lib/menus/package_menus/packages_sub_menu.py
@@ -2,6 +2,7 @@ from models.package import Package
 from rich import print
 from menu_helpers.menu_utils import clear_screen
 import menus.package_menus.packages_main_menu  # packages_main_menu()
+import menus.package_menus.package_crud_menu  # package_crud_menu
 import pyfiglet
 
 
@@ -12,7 +13,7 @@ def packages_sub_menu(package):
     clear_screen()
     print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
     while True:
-        packages_main_menu_options(package)
+        packages_sub_menu_options(package)
         choice = input("--> ")
         if ((choice.lower() == "b") or (choice.lower() == "back")):
             menus.package_menus.packages_main_menu.packages_main_menu()
@@ -22,7 +23,7 @@ def packages_sub_menu(package):
                   "[red blink][bold]Error:[/bold] Invalid option, try again.")
 
 
-def packages_main_menu_options(package):
+def packages_sub_menu_options(package):
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
     print(f"[magenta]*[/magenta]  [dark_turquoise]{package.name}")
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
@@ -30,6 +31,11 @@ def packages_main_menu_options(package):
     print('[dark_turquoise]*[/dark_turquoise]  [bold][dark_turquoise]"back" [magenta][italic]OR[/italic][/magenta] "b"[/bold][/dark_turquoise][grey53] to return to the previous menu',
           ':right_arrow_curving_left:')
     print(
-        '[grey53]*[/grey53]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a row', ':robot:')
+        '[grey53]*[/grey53]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
+    print(
+        '[dark_turquoise]*[/dark_turquoise]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
+    print(
+        '[grey53]*[/grey53]  [bold][grey53]NUM[/bold][/grey53][dark_turquoise] to modify a package', ':robot:')
     print("[dark_turquoise]*[/dark_turquoise]")
+    print("[grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise]")
     print("[grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise]")
