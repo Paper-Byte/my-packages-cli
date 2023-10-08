b703df07a9dd4d1fb5c6c1bd3a830f9926766e87
Noah <noahhughesph@gmail.com>
2023-10-07 21:49:50 -0400

    Update language name in languages_name_menu.py

--- lib/menus/language_menus/languages_name_menu.py
+++ lib/menus/language_menus/languages_name_menu.py
@@ -1,44 +1,45 @@
 from rich import print
 from menu_helpers.menu_utils import clear_screen
-import menu_helpers.packages_menu_helpers  # update_package_name()
-import menus.package_menus.packages_sub_menu  # packages_sub_menu()
+import menu_helpers.language_menu_helpers
+import menus.language_menus.languages_sub_menu
 import pyfiglet
 
 
-def packages_name_menu(package):
+def languages_name_menu(language):
 
-    PACKAGE_TITLE = pyfiglet.figlet_format((f'{package.name}'), font='mini')
+    LANGUAGE_TITLE = pyfiglet.figlet_format((f'{language.name}'), font='mini')
 
     clear_screen()
-    print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
+    print(f"[bold dark_turquoise]{LANGUAGE_TITLE}")
     while True:
-        packages_name_menu_options(package)
+        languages_name_menu_options(language)
         choice = input("NAME :> ")
         if ((choice.lower() == "b") or (choice.lower() == "back")):
-            menus.package_menus.packages_sub_menu.packages_sub_menu(package)
+            menus.language_menus.languages_sub_menu.languages_sub_menu(
+                language)
         elif (not choice.isnumeric()):
             if (len(choice) <= 20):
-                menu_helpers.packages_menu_helpers.update_package_name(
-                    package, choice)
+                menu_helpers.language_menu_helpers.update_language_name(
+                    language, choice)
             else:
-                print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
+                print(f"[bold dark_turquoise]{LANGUAGE_TITLE}")
                 print(":white_exclamation_mark:",
                       "[red blink][bold]Error:[/bold] Invalid name, try again.")
         else:
-            print(f"[bold dark_turquoise]{PACKAGE_TITLE}")
+            print(f"[bold dark_turquoise]{LANGUAGE_TITLE}")
             print(":white_exclamation_mark:",
                   "[red blink][bold]Error:[/bold] Invalid command, try again.")
 
 
-def packages_name_menu_options(package):
+def languages_name_menu_options(language):
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
     print(
-        f"[magenta]*[/magenta]  [dark_turquoise]Name: [/dark_turquoise][grey53]{package.name}")
+        f"[magenta]*[/magenta]  [dark_turquoise]Name: [/dark_turquoise][grey53]{language.name}")
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
     print("[grey53]*[/grey53]")
     print('[dark_turquoise]*[/dark_turquoise]  [bold][dark_turquoise]"back" [magenta][italic]OR[/italic][/magenta] "b"[/bold][/dark_turquoise][grey53] to return to the previous menu',
           ':right_arrow_curving_left:')
     print(
-        '[grey53]*[/grey53]  [bold][dark_turquoise]INPUT[/bold][/dark_turquoise][grey53] to change the package name [italic]*max 20 chars / min 1 char*[/italic]', ':robot:')
+        '[grey53]*[/grey53]  [bold][dark_turquoise]INPUT[/bold][/dark_turquoise][grey53] to change the language name [italic]*max 20 chars / min 1 char*[/italic]', ':robot:')
     print("[dark_turquoise]*[/dark_turquoise]")
     print("[grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise]")
