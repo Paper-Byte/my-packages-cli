e43b794bf0ba24762ee2ef88a6b48a0b0e9b750c
Noah <noahhughesph@gmail.com>
2023-10-08 01:38:33 -0400

    Refactor packages_directory_menu and packages_install_confirm_menu

--- lib/menus/package_menus/packages_directory_menu.py
+++ lib/menus/package_menus/packages_directory_menu.py
@@ -1,6 +1,5 @@
 from ascii_titles import INSTALL_PACKAGE
 from models.language import Language
-from models.package import Package
 from rich import print
 from menu_helpers.menu_utils import clear_screen
 import menus.package_menus.packages_install_choice_menu
@@ -9,21 +8,26 @@ import menus.package_menus.packages_install_confirm_menu
 
 def packages_directory_menu(package):
 
+    package_language = Language.find_by_id(package.language_id)
+
     clear_screen()
     print(f"[bold dark_turquoise]{INSTALL_PACKAGE}")
     while True:
-        packages_directory_menu_options(package)
+        packages_directory_menu_options(package, package_language)
         choice = input("FILEPATH :> ")
-        if (not choice.isnumeric()):
-            pass
+        if ((choice.lower() == "b") or (choice.lower() == "back")):
+            menus.package_menus.packages_install_choice_menu.packages_install_choice_menu()
+        if ((not choice.isnumeric()) and (len(choice) >= 10)):
+            directory = choice
+            menus.package_menus.packages_install_confirm_menu.packages_install_confirm_menu(
+                package, package_language, directory)
         else:
             print(f"[bold dark_turquoise]{INSTALL_PACKAGE}")
             print(":white_exclamation_mark:",
                   "[red blink][bold]Error:[/bold] Invalid path, try again.")
 
 
-def packages_directory_menu_options(package):
-    package_language = Language.find_by_id(package.language_id)
+def packages_directory_menu_options(package, package_language):
     print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
     print(
         f"[magenta]*[/magenta]  [dark_turquoise]Package: [/dark_turquoise][grey53]{package.name}")
---
+++ lib/menus/package_menus/packages_install_confirm_menu.py
@@ -0,0 +1,39 @@
+from ascii_titles import INSTALL_PACKAGE
+from rich import print
+from menu_helpers.menu_utils import clear_screen
+from menu_helpers.packages_menu_helpers import install_package
+import menus.package_menus.packages_directory_menu
+
+
+def packages_install_confirm_menu(package, package_language, directory):
+    clear_screen()
+    print(f"[bold][dark_turquoise]{INSTALL_PACKAGE}")
+    while True:
+        packages_install_confirm_menu_options(
+            package, package_language, directory)
+        choice = input("Y/N :> ")
+        if (choice.lower() == "n"):
+            menus.package_menus.packages_directory_menu.packages_directory_menu(
+                package)
+        elif (choice.lower() == 'y'):
+            pass
+        else:
+            print(f"[bold dark_turquoise]{INSTALL_PACKAGE}")
+            print(":white_exclamation_mark:",
+                  "[red blink][bold]Error:[/bold] Invalid option, try again.")
+
+
+def packages_install_confirm_menu_options(package, package_language, directory):
+    print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
+    print(
+        f"[magenta]*[/magenta]  [dark_turquoise]Package: [/dark_turquoise][grey53]{package.name}")
+    print(
+        f"[magenta]*[/magenta]  [dark_turquoise]Language: [/dark_turquoise][grey53]{package_language.name}")
+    print(
+        f"[magenta]*[/magenta]  [dark_turquoise]Directory: [/dark_turquoise][grey53]{directory}")
+    print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
+    print("[grey53]*[/grey53]")
+    print(
+        '[dark_turquoise]*[/dark_turquoise]  [bold][dark_turquoise]Y/N[/bold][/dark_turquoise][grey53] is this correct?', ':robot:')
+    print("[grey53]*[/grey53]")
+    print("[dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53][dark_turquoise]*[/dark_turquoise][grey53]*[/grey53]")
